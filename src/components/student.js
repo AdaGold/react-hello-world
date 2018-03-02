33184599454ad4cc7909762263da78db67c41b79
Kari Bancroft <kariabancroft@gmail.com>
2018-03-02 14:47:57 -0800

    add JSX contents to the student component

--- src/components/student.js
+++ src/components/student.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
 class Student extends Component {
-
+  render() {
+    return (
+      <div>
+        <strong>Name:</strong>Ada
+        <br />
+        <strong>E-mail:</strong>ada@ada.co
+      </div>
+    )
+  }
 }
 
 export default Student;
