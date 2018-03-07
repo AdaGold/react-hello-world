3b4fe4bd231caebf7379a5d059e33803c3c56683
Kari Bancroft <kariabancroft@gmail.com>
2018-03-07 15:16:03 -0800

    student collection renders individual students for each piece of student data

--- src/components/student_collection.js
+++ src/components/student_collection.js
@@ -3,9 +3,32 @@ import React, { Component } from 'react';
 import Student from './student';
 
 class StudentCollection extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      students: [
+        {
+          name: "State Ada",
+          email: "state-ada@ada.co"
+        },
+        {
+          name: "Cool",
+          email: "cool@ada.co"
+        }
+      ]
+    };
+  }
   render() {
+    let studentComponents = this.state.students.map(function(student) {
+      return <Student name={ student.name } email={ student.email }/>
+    });
+
     return (
-      <Student name="Improved Ada" email="improved-ada@ada.co"/>
+      <div>
+        <h3>Students</h3>
+        { studentComponents }
+      </div>
     )
   }
 }
